224d230574be00a11c690784bbbc72f7460edadd
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-04-07 16:24:44 -0500

    update test

--- PyBank/main_test.py
+++ PyBank/main_test.py
@@ -1,22 +1,40 @@
 import os
 import csv
-print(os.getcwd())
-# Need to import csv files and pathway (DONE)
-# Need to get total number of months (DONE)
+# print(os.getcwd()) 'test for pathway'
+
+# Need to import csv files and pathway
+# Need to get total number of months
 # Need to get net total amount of 'Profit/Losses'
 # Need to get average of changes of 'Profit/Loses'
 # Need to get greatest increase in profits (date and amount) over entire period
 # Need to get greatest decrease in loses (date and amount) over entire period
 
+
+# Defined the pathway
 pybank_csv = os.path.join('Resources', 'budget_data.csv')
+
+# Defining the function for 'bank_data'
+def print_totals(bank_data):
+    months = str(bank_data[0])
+    pft_ls = int(bank_data[1])
+
+# Defining the calculations
+    total_months = months - 1
+
+    net = sum(pft_ls)
+
+    avg = sum(pft_ls) / len(pft_ls)
+
+    max_pft = max(pft_ls)
+
+    min_pft = min(pft_ls)
+
+    avg_chg = (max_pft - min_pft) / avg
+    
+
+
+
+# Read in the pathway
 with open(pybank_csv, "r") as file:
     reader = csv.reader(file)
-    total_months = len(list(reader))
-
-    print(f"Total months: {total_months}")
-
-
-    #def print_analysis(bank_data):
-
-    #date = str(bank_data[0])
-    #pft_lss = int(bank_data[1])
+    total_months_total = len(list(reader))
--- PyBank/main_test_pd.py
+++ PyBank/main_test_pd.py
@@ -9,3 +9,5 @@ bank_df = pd.read_csv(csv_path)
 bank_df["Date"].count()
 
 bank_df["Profit/Losses"].sum()
+
+bank_df["Profit/Losses"].mean()
