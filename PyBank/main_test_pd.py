c789419b5509f605f3e2c4d39b79e7651d9c6d8b
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-04-07 22:10:42 -0500

    upgrade save

--- PyBank/main_test_pd.py
+++ PyBank/main_test_pd.py
@@ -1,13 +1,15 @@
+# Make sure to use print() to have python print dataframe
+
 import pandas as pd
 
 csv_path = "Resources/budget_data.csv"
 
 bank_df = pd.read_csv(csv_path)
 
-# bank_df.head()
+print(bank_df.head())
 
-bank_df["Date"].count()
+# bank_df["Date"].count()
 
-bank_df["Profit/Losses"].sum()
+# bank_df["Profit/Losses"].sum()
 
-bank_df["Profit/Losses"].mean()
+# bank_df["Profit/Losses"].mean()
